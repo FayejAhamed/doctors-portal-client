aefb76090d40609dc4349729d441800a168c9776
my name <myemailid@domain.com>
2022-05-19 11:54:37 -0400

    loging done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import About from './Pages/About/About';
 import Appointment from './Pages/Appointment/Appointment';
 import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
+import RequireAuth from './Pages/Login/RequireAuth';
+import SignUp from './Pages/Login/SignUp';
 import Navbar from './Pages/Shared/Navbar';
 
 function App() {
@@ -14,8 +16,14 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
+        <Route path="appoinment" element={
+          <RequireAuth>
+            <Appointment />
+          </RequireAuth>
+        } />
         <Route path="login" element={<Login />} />
-        <Route path="appoinment" element={<Appointment />} />
+        <Route path="signup" element={<SignUp />} />
+
       </Routes>
     </div>
   );
